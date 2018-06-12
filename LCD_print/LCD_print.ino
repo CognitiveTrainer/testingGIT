c6e78fcac87d7c5029832963ce74f630bcde253d
Federico Bello <federico.bello@outlook.com>
2018-06-12 16:03:58 -0300

    LCD Print working

--- LCD_print/LCD_print.ino
+++ LCD_print/LCD_print.ino
@@ -2,24 +2,32 @@
 
 #include <Wire.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 void setup() {
-
-  Serial.begin(115200);
-
-  //Use predefined PINS consts
-
-  Wire.begin(D2, D1);
-
- // The begin call takes the width and height. This
- // Should match the number provided to the constructor.
+  // The begin call takes the width and height. This
+  // Should match the number provided to the constructor.
   lcd.begin(16,2);
-
+  lcd.init();
+  
   //Prender luz de fondo
-
   lcd.backlight();
 
+}
+
+void loop() {
+  
+  lcdLeerJugador();
+  delay(1000);
+  lcdJugadorLeido();
+  delay(1000);
+}
+
+void lcdLeerJugador(){
+  //Limpiamos pantalla
+  lcd.init();
+  lcd.clear();
+  
   lcd.home();
 
   lcd.print("Escanee tarjeta");
@@ -29,10 +37,22 @@ void setup() {
   lcd.setCursor(0, 1);
   
   lcd.print("de jugador...");
-
 }
 
-void loop() { 
-  // do nothing here 
+void lcdJugadorLeido(){
+  //Limpiamos pantalla
+  lcd.init();
+  lcd.clear();
+  
+  lcd.home();
+
+  lcd.print("Se leyo el");
+  
+  // Move the cursor characters to the right and
+  // zero characters down (line 1).
+  lcd.setCursor(0, 1);
+  
+  lcd.print("deportista: 12345");
 }
+
 
