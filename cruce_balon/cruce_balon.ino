f5e91ff9511e5a6a0995639386b0e926654c548f
Federico Bello <federico.bello@outlook.com>
2018-07-15 22:53:19 -0300

    Cruce balon con simple y simon

--- cruce_balon/cruce_balon.ino
+++ cruce_balon/cruce_balon.ino
@@ -19,13 +19,11 @@
 //    static const uint8_t D9   = 3;
 //    static const uint8_t D10  = 1;
 
-// How many leds in your strip?
+// Cantidad de leds por tira y numero total de tiras
 #define NUM_LEDS 40
 #define NUM_STRIPS 3
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Definimos pines de sensores y tiras
 #define SENSOR_PIN_1 16 // D0
 #define SENSOR_PIN_2 5  // D1
 #define SENSOR_PIN_3 4  // D2
@@ -34,12 +32,23 @@
 #define STRIP_PIN_2 2   // D4
 #define STRIP_PIN_3 14  // D5
 
-#define CLOCK_PIN   13
-
+// Definimos brillo
 #define BRIGHTNESS  64
 
 // Define the array of leds
 CRGB LEDS_STRIPS[NUM_STRIPS][NUM_LEDS];
+
+// Variables para cualquier entrenamiento
+int timeStart = 0;
+int timeStop = 0;
+int timeTotal = 0;
+
+// Variables para entrenamiento Simon
+const int sequenceMax = 5;
+int sequence[sequenceMax];
+int sequenceUser[sequenceMax];
+const int timeTotalShow=2000;
+int level;
 
 void setup() { 
       // 
@@ -53,15 +62,20 @@ void setup() {
 }
 
 void loop() { 
+  //cruceSimple();
+  cruceSimon();
+}
 
+// ----------------------------------------------------------------
+// -- Funcion cruceSimple() para el modo de entrenamiento Simple --
+// ----------------------------------------------------------------
+
+void cruceSimple(){
   Serial.print("Comienzo");
     
   randomSeed(millis());
   int numAleatorio = random(3);
   
-  int timeStart = 0;
-  int timeStop = 0;
-  int timeTotal = 0;
   int cruce = -1;
   int correcto = 0;
   int error = 0;
@@ -99,7 +113,10 @@ void loop() {
     } else if(digitalRead(SENSOR_PIN_3)==0){
       cruce=2;
     }
-  }  
+  }
+  turnOnBlue(cruce);
+  FastLED.show();
+  tiempoEsperaLed(400); 
   // Guardamos el tiempo que tardó en finalizar el juego
   timeTotal = millis() - timeStart;
   int timeTotalSec = timeTotal/1000;
@@ -124,14 +141,177 @@ void loop() {
   delay(500);
 }
 
+// --------------------------------------------------------------
+// -- Funcion cruceSimon() para el modo de entrenamiento Simon --
+// --------------------------------------------------------------
+
+void cruceSimon(){
+  Serial.println("Simon entro");
+  bool error=false;
+  
+  // Pongo nivel actual en 0
+  level=0;
+  
+  generateSequence();
+  
+  while(level<=sequenceMax && error==false){
+    level++;
+    Serial.print("Nivel: ");
+    Serial.println(level);
+    showSequence();
+    error = getSequence();
+  }
+  if(error){
+    endGame();
+  }
+}
+
+void generateSequence()
+{
+  Serial.print("Generar Secuencia...");
+  randomSeed(millis()); //in this way is really random!!!
+  for (int i=0; i<sequenceMax; i++){
+    sequence[i] = random(3);
+    Serial.println(sequence[i]);
+  } 
+}
+
+void showSequence(){
+  Serial.print("Mostrar secuencia");
+  for (int i=0; i<level; i++){
+    showWhiteAll();
+    tiempoEsperaLed(1000);
+    Serial.println(sequence[i]);
+    if(sequence[i]==0){
+      turnOnRed(0);
+      turnOnWhite(1);
+      turnOnWhite(2);
+    } else if(sequence[i]==1){
+      turnOnWhite(0);
+      turnOnGreen(1);
+      turnOnWhite(2);
+    } else if(sequence[i]==2){
+      turnOnWhite(0);
+      turnOnWhite(1);
+      turnOnBlue(2);
+    }
+    FastLED.show();
+    tiempoEsperaLed(1000);
+  }
+}
+
+bool getSequence(){
+  int sequenceRead=0;
+  bool error=false;
+  
+  Serial.println("Obtener Secuencia...");
+  
+  while(sequenceRead<level && error==false){
+    bool flag = false;
+    
+    ESP.wdtFeed();
+    
+    turnOnYellow(0);
+    turnOnYellow(1);
+    turnOnYellow(2);
+    FastLED.show();
+    
+    Serial.println("Secuencia: ");
+    Serial.println(sequenceRead);
+    Serial.println("Nivel: ");
+    Serial.println(level);
+    
+    int cruce=-1;
+    while(flag==false){
+      ESP.wdtFeed();
+      if(digitalRead(SENSOR_PIN_1)==0){
+        cruce=0;
+        turnOnRed(0);
+        flag=true;
+      } else if(digitalRead(SENSOR_PIN_2)==0){
+        cruce=1;
+        turnOnGreen(1);
+        flag=true;
+      } else if(digitalRead(SENSOR_PIN_3)==0){
+        cruce=2;
+        turnOnBlue(2);
+        flag=true;
+      }
+    }
+    FastLED.show();
+    tiempoEsperaLed(400);
+    if(cruce==sequence[sequenceRead]){
+      sequenceRead++;      
+    } else {
+      error=true;
+      return error;
+    }
+  } 
+  return error;
+}
+
 void turnOnGreen(int pos){
+  //Serial.println("Turn on green");
   for(int i=0;i<NUM_LEDS;i++)
     LEDS_STRIPS[pos][i] = CRGB::Green;
 }
 
-void turnOnRed(int pos){    
+void turnOnRed(int pos){  
+  //Serial.println("Turn on red");  
   for(int i=0;i<NUM_LEDS;i++)
     LEDS_STRIPS[pos][i] = CRGB::Red;
 }
 
+void turnOnBlue(int pos){    
+  //Serial.println("Turn on blue");
+  for(int i=0;i<NUM_LEDS;i++)
+    LEDS_STRIPS[pos][i] = CRGB::Blue;
+}
+
+void turnOnWhite(int pos){
+  //Serial.println("Turn on white");
+  for(int i=0;i<NUM_LEDS;i++)
+    LEDS_STRIPS[pos][i] = CRGB::White;
+}
+
+void turnOnYellow(int pos){
+  //Serial.println("Turn on yellow");
+  for(int i=0;i<NUM_LEDS;i++)
+    LEDS_STRIPS[pos][i] = CRGB::Yellow;
+}
+
+void turnOnBlack(int pos){
+  //Serial.println("Turn on black");
+  for(int i=0;i<NUM_LEDS;i++)
+    LEDS_STRIPS[pos][i] = CRGB::Black;
+}
+
+void showWhiteAll(){
+  turnOnWhite(0);
+  turnOnWhite(1);
+  turnOnWhite(2);
+  FastLED.show();
+}
+
+void endGame(){
+  for(int i=0;i<3;i++){
+    turnOnRed(0);
+    turnOnRed(1);
+    turnOnRed(2);
+    FastLED.show(); 
+    tiempoEsperaLed(250);
+    turnOnBlack(0);
+    turnOnBlack(1);
+    turnOnBlack(2);
+    FastLED.show(); 
+    tiempoEsperaLed(250);
+  }
+}
+
+void tiempoEsperaLed(int stopTimeWait){
+  int startTimeWait = millis();
+  while(stopTimeWait>millis()-startTimeWait){
+    ESP.wdtFeed();
+  }
+}
 
